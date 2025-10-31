13fc8a7cc5119b015dfc782a3a3a5b19e7d6434a
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 11:00:46 +0530

    Experiment-4.3.7.c with number pattern
    Add a C program to print a pattern of numbers.

---
+++ Experiment-4.3.7.c
@@ -0,0 +1,16 @@
+/*4.3.7*/ 
+int main(){  
+int i,k,j;  
+printf("Name – Aditya Bisht\nSAP ID - 590028757 \nCourse - BCA \nBatchB6");  
+printf("\n----------------------------------------------------------------\n");  
+for(i=5;i>=1;i--){    
+for(k=5;k>=i;k--){  
+}  
+// space  
+printf(" ");  
+for(j=1;j<=i;j++){  
+printf("%d",j);  
+}  
+printf("\n");  
+}  
+} 
