8c6ff0f2f675a560022f480851ee14af626033dc
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:52:21 +0530

    Add multiplication table program in Experiment-4.2.c
    This program prints the multiplication table for a user-entered number with proper formatting.

---
+++ Experiment-4.2.c
@@ -0,0 +1,17 @@
+ 4.2. WAP to print the multiplication table of the number entered by the user. It should be in the correct formatting. Num * 1 = Num 
+
+#include <stdio.h> 
+int main() { 
+printf("Name-Aditya Bisht\nSAP ID
+590028757\nCourse-BCA\nBatch-B6"); 
+printf("\n-------------------------------------------\n"); 
+int num; 
+printf("Enter a number to print its multiplication 
+table: "); 
+scanf("%d", &num); 
+printf("\nMultiplication Table of %d:\n", num); 
+for (int i = 1; i <= 10; i++) { 
+printf("%d * %d = %d\n", num, i, num * i); 
+} 
+return 0; 
+}
