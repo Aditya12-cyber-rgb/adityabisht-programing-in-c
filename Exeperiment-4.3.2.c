2fba21f53b77443d656c4dee7c3f66f003e85ce6
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:56:12 +0530

    Add C program to print pattern and user details
    This program prints a pattern based on the value of n, along with user details.

---
+++ Exeperiment-4.3.2.c
@@ -0,0 +1,13 @@
+/*4.3.2*/
+int main(){  
+int 
+int n=1;  
+i,j,k;  
+printf("Name – Aditya bisht \nSAP ID - 590028757 \nCourse - BCA \nBatchB6");  printf("\n----------------------------------------------------------------\n");  
+for(i=0;i<=4;i++){  
+for(j=0;j<=i;j++){  
+printf("%d",n+j);  
+}  
+printf("\n");  
+}  
+} 
