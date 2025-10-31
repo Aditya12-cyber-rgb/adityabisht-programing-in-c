4c1cb1c7fe0c281140a91a98e937a1da75d090c9
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:57:45 +0530

    Add main function to print details and number pattern
    This program prints a name, SAP ID, course, and batch, followed by a pattern of numbers.

---
+++ Experiment-4.3.4.c
@@ -0,0 +1,13 @@
+/*4.3.4*/ 
+int main(){  
+int i,j;  
+printf("Name – Aditya bisht \nSAP ID - 590028757 \nCourse - BCA \nBatch- 
+B6");  
+}  
+printf("\n----------------------------------------------------------------\n");  
+for(i=1;i<=5;i++){  
+for(j=i;j>=1;j--){  
+printf("%d",j);  
+}  
+printf("\n");  
+}  
