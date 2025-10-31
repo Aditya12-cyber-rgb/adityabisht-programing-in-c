9379dfe3191e9565e8994cc7acc19199f3c7780a
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:58:28 +0530

    Create Experiment-4.3.5.c with pattern printing
    Add a C program that prints a pattern based on user-defined input.

---
+++ Experiment-4.3.5.c
@@ -0,0 +1,17 @@
+/*4.3.5*/ 
+int main(){  
+int i,j,k;  
+printf("Name – Aditya bisht\nSAP ID - 590028757 \nCourse - BCA \nBatchB6");  
+printf("\n----------------------------------------------------------------\n");  
+for(i=1;i<=5;i++){  
+//  
+for(k=i;k<=4;k++){  
+for(k=4;k>=i;k--){      
+printf(" ");  
+}  
+for(j=1;j<=i;j++){  
+printf("%d",j);  
+}  
+printf("\n");  
+}  
+} 
