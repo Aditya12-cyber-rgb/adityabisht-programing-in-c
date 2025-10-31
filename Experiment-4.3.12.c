49508158f9e30da88074c8f960ec4830ba0680ef
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 11:04:19 +0530

    Fix formatting of printf statements in Experiment-4.3.12.c

---
+++ Experiment-4.3.12.c
@@ -0,0 +1,15 @@
+/*4.3.12*/
+int main(){  
+int i,j,k;  
+printf("Name – Aditya Bisht\nSAP ID - 590028757 \nCourse - BCA \nBatchB6");  
+  printf("\n----------------------------------------------------------------\n");  
+for(i=1;i<=5;i++){  
+for(k=1;k<=i;k++){  
+printf(" ");  
+}  
+for(j=5;j>=i;j--){  
+printf("%d",j);  
+}  
+printf("\n");  
+}  
+} 
