76452c54d0f039664c584eba5e66842ccbf65cb4
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 11:13:32 +0530

    Add C program to count frequency of a number
    Implement a C program to find the frequency of a number in an array.

---
+++ Experiment-6.3.c
@@ -0,0 +1,22 @@
+6.3. WAP to read a list of integers and store it in a single dimensional array. Write a C program to find the frequency of a particular number in a list of integers. 
+
+#include <stdio.h> 
+int main() { 
+printf("Name-AdityaBisht\nSAPID-590028757\nCourse-BCA\nBatch-B6"); 
+printf("\n------------------------------------------------------\n"); 
+int n, i, num, count=0; 
+printf("Enter number of elements: "); 
+scanf("%d", &n); 
+int arr[n]; 
+printf("Enter %d integers:\n", n); 
+for(i=0; i<n; i++) 
+scanf("%d", &arr[i]); 
+printf("Enter number to find frequency: "); 
+scanf("%d", &num); 
+for(i=0; i<n; i++) { 
+if(arr[i] == num) 
+count++; 
+} 
+printf("Frequency of %d = %d\n", num, count); 
+return 0; 
+} 
