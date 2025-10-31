51fafca88e30eac99efe957c883f6ecf2b65ed14
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:45:31 +0530

    Add C program to check if a number is even or odd

---
+++ Experiment-3.1.c
@@ -0,0 +1,19 @@
+//Experiment 3: Conditional Statements 
+//3.1.? Write a C program to check whether a number is Even or ODD 
+
+#include<stdio.h> 
+int main(){ 
+printf("Name- Aditya Bisht\nSAP-ID
+590028757\nCourse-BCA\nBatch-B6"); 
+printf("\n-----------------------------------------------------\n"); 
+int number; 
+printf("Enter an integer: "); 
+scanf("%d", &number); 
+if (number % 2 == 0) { 
+printf("%d is Even.\n", number); 
+} else { 
+printf("%d is Odd.\n", number); 
+} 
+return 0; 
+} 
+//Experiment 3: Conditional
