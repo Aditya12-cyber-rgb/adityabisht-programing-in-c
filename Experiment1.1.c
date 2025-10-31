70d52543a0812e234f47e9c568e5bab003911c5d
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:36:44 +0530

    Experiment1.1.c with Hello World program
    Added a C program that prints user details and 'Hello World'.

---
+++ Experiment1.1.c
@@ -0,0 +1,11 @@
+Experiment 1: Installation, Environment Setup, and starting with the C Language
+
+1)Write a C program to print "Hello World"
+SOLUTION:
+#include<stdio.h>
+int main(){
+	printf("Name-Aditya Bisht\nSAP_ID-590028757\nCourse-BCA\nBatch-6");
+	printf("\n-----------------------------------------------------\n");
+	printf("Hello World");
+	return 0;
+}
