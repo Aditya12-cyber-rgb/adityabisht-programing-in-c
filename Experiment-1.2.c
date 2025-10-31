a338c5627110cf354680c2f4b091de6dc0f248c3
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:38:13 +0530

    Add C program to print address in multiple lines
    This program prints personal details and address in multiple lines.

---
+++ Experiment-1.2.c
@@ -0,0 +1,14 @@
+2)Write a C program to print the address in multiple lines(new line).
+SOLUTION:
+
+#include<stdio.h>
+int main(){
+	printf("Name-Aditya Bisht\nSAP_ID-590028757\nCourse-BCA\nBatch-6");
+	printf("\n-----------------------------------------------------\n");
+	printf("House No:05\n");
+	printf("Street : Hariyawala Kalan\n");
+	printf("City : Dehradun\n");
+	printf("State : Uttarakhand\n");
+	printf("Pincode : 248141");
+	return 0;
+}
