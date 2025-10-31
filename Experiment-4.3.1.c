104c7bc5150176f6e624618e1ac4c498867d194f
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:55:10 +0530

    Add pattern printing program in Experiment-4.3.1.c
    Implement a program to print a pattern using loops.

---
+++ Experiment-4.3.1.c
@@ -0,0 +1,19 @@
+Experiment 4: LOOPS   
+4.3. Write Saparate programs for each patterns and number then  4.3.1, 4.3.2, 4.3.3,and so on 
+
+/*4.3.1*/ int main(){  
+  int i,j;  
+  printf("Name – Aditya Bisht \nSAP ID - 590028757 \nCourse - BCA \nBatch-B6");  
+  printf("\n----------------------------------------------------------------\n");  
+  
+  for(i=1;i<=5;i++){  
+    
+    for(j=1;j<=i;j++){  
+      printf("%d",i);  
+        
+    }  
+      printf("\n");  
+    
+}  
+  
+} 
