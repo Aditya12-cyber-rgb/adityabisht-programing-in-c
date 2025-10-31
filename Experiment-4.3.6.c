1951639d7c87ebb851d5fb0a3d7f85084693a6a9
Aditya12-cyber-rgb <adityabisht705@gmail.com>
2025-10-31 10:59:20 +0530

    Add Experiment-4.3.6.c with main function

---
+++ Experiment-4.3.6.c
@@ -0,0 +1,11 @@
+/*4.3.6*/ 
+int main(){  
+int i,j;  
+printf("Name – Aditya bisht \nSAP ID - 590028757 \nCourse - BCA \nBatchB6");  printf("\n----------------------------------------------------------------\n");  
+for(i=5;i>=1;i--){    
+for(j=i;j>=1;j--){  
+}  
+printf("%d",j);  
+printf("\n");  
+}  
+} 
